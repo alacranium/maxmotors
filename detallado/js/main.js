24c2e5ca755a6cdeaff077fa1904ae09d813edbe
Arturo Ruiz <13041651@itdurango.edu.mx>
2017-10-25 16:15:40 -0500

    fierro pariente

--- detallado/js/main.js
+++ detallado/js/main.js
@@ -14,7 +14,25 @@ var main = function() {
     arrows: false,
     autoplaySpeed: 2000
   });
-};
+
+
+var width = document.body.clientWidth;
+
+document.getElementById("screen_size").innerHTML = width;
+
+  if (width < 834) 
+  {
+    $(detalles).css("position", "fixed");
+    $(detalles).css("top", "100px");
+    
+  }
+  //if (width > 834) 
+  //  {
+    //  $(detalles).css("position", "absolute");
+    //}
+      
+    };
+
 
 
 $(document).ready(main);
